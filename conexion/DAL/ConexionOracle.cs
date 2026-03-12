0dc14bbd9544afdd79a44bb51ac1543fafd8ae25
Kendall Zeledón Sánchez <kzeledon@dcicr.com>
2026-03-11 21:59:09 -0600

    Se adiciona código de conexión.

---
+++ conexion/DAL/ConexionOracle.cs
@@ -0,0 +1,16 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace MotoRepuestosCR.DAL
+{
+    public class ConexionOracle
+    {
+        private readonly string cadenaConexion = "User Id=system;Password=1234;Data Source=localhost:1521/XEPDB1;";
+
+        public OracleConnection ObtenerConexion()
+        {
+            OracleConnection cn = new OracleConnection(cadenaConexion);
+            cn.Open();
+            return cn;
+        }
+    }
+}
---
+++ conexion/FrmPrincipal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MotoRepuestosCR.DAL;
+using Oracle.ManagedDataAccess.Client;
+
+namespace MotoRepuestosCR
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+
+            try
+            {
+                ConexionOracle conexion = new ConexionOracle();
+                using (OracleConnection cn = conexion.ObtenerConexion())
+                {
+                    MessageBox.Show("Conexión exitosa a Oracle.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de conexión: " + ex.Message);
+            }
+        }
+    }
+}
